f25f7ee798206579735d43bbe28bfe53f648f6fc
despliegueterritorial <78526573+despliegueterritorial@users.noreply.github.com>
2021-02-04 03:30:27 -0300

    Update start.js

--- start.js
+++ start.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 
+const DBurl  = (process.env.DATABASE_URL || 'no DB!!');
 const port = process.env.PORT || 80;
 
 app.get('/', function (req, res) {
-  res.send('Working!');
+  res.send('Working! DB: ' + DBurl);
 });
 app.listen(port, function () {
   console.log('Event app listening on port: ' + port);
